77aef4004ab9c32c955d0bbe0bfe17916ba95d89
Wen Li <wenli@Wen-Li-MacBook-Pro.local>
2015-08-07 14:50:42 -0400

    Query optimization applied

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -9,10 +9,14 @@ class ReportsController < ApplicationController
   def find_total_volume
     _genre_to_volume = Hash.new(0)
     _subgenre_to_volume = Hash.new{ |h, k| h[k]=Hash.new(&h.default_proc) }
-    Genre.find_each do |genre_elem|
+
+    genres = Genre.includes(:subgenres)
+
+    genres.each do |genre_elem|
       _genre_to_volume[genre_elem.name] = genre_elem.count_books_in_genre
       genre_elem.subgenres.each do |subgenre_elem|
-        _subgenre_to_volume[genre_elem.name]["#{subgenre_elem.name}_#{subgenre_elem.id.to_s}"] = subgenre_elem.count_books_in_subgenre
+        _subgenre_to_volume[genre_elem.name]["#{subgenre_elem.name}_#{subgenre_elem.id.to_s}"] =
+          subgenre_elem.count_books_in_subgenre
       end
     end
     [_genre_to_volume, _subgenre_to_volume]
@@ -21,7 +25,10 @@ class ReportsController < ApplicationController
   def generate_sales_history
     sales_history_quantity = Hash.new(0)
     sales_history_revenue = Hash.new(0)
-    Book.find_each do |book|
+
+    books = Book.includes(:order_details)
+
+    books.each do |book|
       book.order_details.each do |od|
         sales_history_quantity["#{book.name}_#{book.id}"] += od.quantity
         sales_history_revenue["#{book.name}_#{book.id}"] += od.quantity * od.unit_price
